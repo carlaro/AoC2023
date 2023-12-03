e390f922d0992baa66d97f48ae9931151998ebf1
Carl <carlaro@kth.se>
2023-12-03 17:21:46 +0100

    solved day2

---
+++ day2.cpp
@@ -0,0 +1,102 @@
+#include <string>
+#include <string_view>
+#include <iostream>
+#include <unordered_map>
+#include <limits>
+#include <thread>
+#include "help_func.hpp"
+
+[[nodiscard]] int problem_1(std::vector<std::string>& input) {
+    std::unordered_map<std::string, int> boundaries{{"red", 12}, {"green", 13}, {"blue", 14}};
+    int sum{};
+
+    for(std::string &s : input) {
+        auto split_vec = help_func::split(s, ": ");
+        
+
+        std::string game_string = split_vec[0];
+        std::size_t from{s.find(" ")};
+        std::size_t to{s.find(":")};
+        std::string game_num = game_string.substr(from + 1, to - from - 1);
+
+        std::string shown_string = split_vec[1];
+        auto sub_games = help_func::split(shown_string, ";");
+
+        bool game_possible = true;
+
+        for(auto sub_game : sub_games)
+        {
+            
+            help_func::trim(sub_game);
+            auto outcomes = help_func::split(sub_game, ",");
+
+            for(auto outcome : outcomes){
+                help_func::trim(outcome);
+                auto split_outcome = help_func::split(outcome, " ");
+                std::string key = split_outcome[1];
+                int value = std::stoi(split_outcome[0]);
+
+                if(value > boundaries[key]) {
+                    game_possible = false;
+                }
+            }
+        }
+        if(game_possible) {
+            sum += std::stoi(game_num);
+        }
+    }
+    return sum;
+} 
+
+[[nodiscard]] int problem_2(std::vector<std::string>& input)
+{
+    std::unordered_map<std::string, int> smallest_occurence;
+    int sum{};
+
+    for(std::string &s : input) {
+        auto split_vec = help_func::split(s, ": ");
+        
+        smallest_occurence["red"] = 0;
+        smallest_occurence["green"] = 0;
+        smallest_occurence["blue"] = 0;
+
+        std::string game_string = split_vec[0];
+        std::size_t from{s.find(" ")};
+        std::size_t to{s.find(":")};
+        std::string game_num = game_string.substr(from + 1, to - from - 1);
+
+        std::string shown_string = split_vec[1];
+        auto sub_games = help_func::split(shown_string, ";");
+
+        for(auto sub_game : sub_games) {
+            
+            help_func::trim(sub_game);
+            auto outcomes = help_func::split(sub_game, ",");
+
+            for(auto outcome : outcomes){
+                help_func::trim(outcome);
+                auto split_outcome = help_func::split(outcome, " ");
+                std::string key = split_outcome[1];
+                int value = std::stoi(split_outcome[0]);
+
+                if(value > smallest_occurence[key]) {
+                    smallest_occurence[key] = value;
+                }
+            }
+        }
+        sum += smallest_occurence["red"]*smallest_occurence["green"]*smallest_occurence["blue"];
+    }
+    return sum;
+}
+
+
+int main() {
+    help_func::Timer t;
+    std::vector<std::string> input = help_func::read_input("input-day-2.txt");
+
+    int answer_1 = problem_1(input);
+    int answer_2 = problem_2(input);
+
+    std::cout << answer_1 << "\n";
+    std::cout << answer_2 << "\n";
+}
--- help_func.hpp
+++ help_func.hpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <vector>
 #include <chrono>
+#include <functional>
+#include <algorithm>
 
 namespace help_func{
     [[nodiscard]] std::vector<std::string> read_input(const char* f_name)
@@ -24,6 +26,35 @@ namespace help_func{
             return lines;
         }
         return std::vector<std::string>(0);
+    }
+
+    [[nodiscard]] std::vector<std::string> split(std::string s, std::string delim) {
+        std::vector<std::string> s_vec{};
+        std::size_t search_from{};
+        std::size_t found_at = s.find(delim, search_from);
+        std::size_t offset = delim.size();
+
+        while(found_at < s.npos) {
+            s_vec.push_back(s.substr(search_from, found_at - search_from));
+            search_from = found_at + offset;
+            found_at = s.find(delim, search_from);
+        }
+        s_vec.push_back(s.substr(search_from));
+
+        return s_vec;
+    }
+
+    void ltrim(std::string& s){
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](auto c) {return !std::isspace(c);}));
+    }
+
+    void rtrim(std::string& s){
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](auto c) {return !std::isspace(c);}).base(), s.end());
+    }
+    
+    void trim(std::string& s) {
+        help_func::ltrim(s);
+        help_func::rtrim(s);
     }
 
     class Timer
